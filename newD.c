08e56d0edb4bf9bf4b8138455e11d3e2517b765a
JosephJaeschke <joeyj987@gmail.com>
2017-10-31 17:41:50 -0400

    working on newD.c

---
+++ newD.c
@@ -0,0 +1,91 @@
+#include <unistd.h>  
+#include <stdio.h>  
+#include <dirent.h>  
+#include <string.h>  
+#include <sys/stat.h>  
+#include <sys/wait.h>
+#include <stdlib.h>  
+  
+int isCSV(char* name)
+{
+	char* temp=strdup(name);
+	char* ext=strrchr(temp,'.');
+	if(ext!=NULL&&strcmp(ext,".csv")==0)
+	{
+		free(temp);
+		return 1;
+	}
+	free(temp);
+	return 0;
+}
+  
+void printdir(char *dir)  
+{  
+	DIR *dp;  
+	struct dirent *entry;  
+	struct stat statbuf;  
+	char* path=malloc(1000);
+	if((dp=opendir(dir))==NULL)
+	{  
+        	return ;  
+    	}  
+    	chdir(dir);  
+	int id;
+	int csvid;
+	int procs=0;
+    	while((entry=readdir(dp))!=NULL)
+	{
+        	lstat(entry->d_name,&statbuf);  
+        	if(S_ISDIR(statbuf.st_mode))
+		{  
+            		if(strcmp(entry->d_name, ".") == 0||strcmp(entry->d_name, "..") == 0)   
+                	{
+				continue;
+			}
+			procs++;
+			id=fork();
+			if(id==0)
+			{
+				dp=opendir(entry->d_name);
+				if(dp==NULL)
+				{
+					return;
+				}
+				strcat(path,entry->d_name);
+				strcat(path,"/");
+				chdir(entry->d_name);
+			}
+        	} 
+		else if(isCSV(entry->d_name)) //fork on each csv to sort
+		{
+			csvid=fork();
+			if(csvid==0)
+			{
+				strcat(path,entry->d_name);
+				strcat(path,"\0");
+				printf("%s\n",path);
+				//sortCSVFile(char* sortBy,char* fileName,char* outDir);
+			}
+		}
+    	} 
+	int i;
+	for(i=0;i<procs;i++)
+	{
+		wait(NULL);
+	}
+    	chdir("..");  
+    	closedir(dp);     
+	free(path);
+}  
+  
+  
+int main(int argc, char *argv[])  
+{ 
+
+    char *topdir = argv[1];  
+    if (argc >= 2)  
+        topdir = argv[1];  
+  
+    printdir(topdir);  
+    return 0; 
+} 
