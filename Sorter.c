0db60e66cbe005365f0b6668a18b88eddfb83585
Anuraag Shankar <raknahsa@gmail.com>
2017-10-31 23:12:25 -0400

    Push working code :D

--- Sorter.c
+++ Sorter.c
@@ -4,6 +4,15 @@
 #include <string.h>
 #include <stdio.h>
 #include "Sorter.h"
+#include <unistd.h>  
+#include <dirent.h>  
+#include <sys/stat.h>  
+#include <sys/wait.h>
+ 
+int p[2];
+int depth=0;
+int indexo;
+
 
 void mergeStr(CSVRow* arr,CSVRow* help, int lptr,int rptr,int llimit,int rlimit,int num)
 {
@@ -409,9 +418,9 @@ void sortCSVFile(char * filename1,char * token, char * outdir){
 	callMe(file_count,type,movies,help);
 	//printf("heyo\n");
 	//printf("\n");
-	char * filename = malloc(60);
+	char * filename = malloc(1000);
 	
-	char * tempp = malloc(70);
+	char * tempp = malloc(1000);
 	strncpy(tempp,filename1,strlen(filename1)-4);
 	tempp[strlen(filename1)-4] = '\0';
 	sprintf(filename, "%s/%s-sorted-%s.csv",outdir,tempp,token);
@@ -444,6 +453,98 @@ void sortCSVFile(char * filename1,char * token, char * outdir){
 	return;
 }
 
+  
+int isCSV(char* name)
+{
+	char* temp=strdup(name);
+	char* ext=strrchr(temp,'.');
+	if(ext!=NULL&&strcmp(ext,".csv")==0)
+	{
+		free(temp);
+		return 1;
+	}
+	free(temp);
+	return 0;
+}
+  
+void printdir(char * token, char *dir, char * outdir, int idp)  
+{  
+	DIR *dp;  
+	struct dirent *entry;  
+	struct stat statbuf;  
+	char* path=malloc(1000);
+	if((dp=opendir(dir))==NULL)
+	{  
+        	kill(getpid(), SIGKILL); 
+    	}  
+    	chdir(dir);  
+	int id;
+	int csvid;
+	int procs=0;
+    	while((entry=readdir(dp))!=NULL)
+	{
+        	lstat(entry->d_name,&statbuf);  
+        	if(S_ISDIR(statbuf.st_mode))
+		{  
+            		if(strcmp(entry->d_name, ".") == 0||strcmp(entry->d_name, "..") == 0)   
+                	{
+				continue;
+			}
+		
+			id=fork();
+			procs++;
+			if(id==0)
+			{
+				write(p[1], &indexo, 1);
+				depth+=1;
+				printf("%d, ", getpid());
+				fflush(stdout);
+				dp=opendir(entry->d_name);
+				if(dp==NULL)
+				{
+					return;
+				}
+				strcat(path,entry->d_name);
+				strcat(path,"/");
+				chdir(entry->d_name);
+			}
+        	} 
+		else if(isCSV(entry->d_name)) //fork on each csv to sort
+		{
+			if(strstr(entry->d_name, "-sorted-") != NULL) {
+				    continue;
+			}
+			csvid=fork();
+			procs++;
+			if(csvid==0)
+			{
+				write(p[1], &indexo, 1);
+				printf("%d, ", getpid());
+				fflush(stdout);
+				strcat(path,entry->d_name);
+				strcat(path,"\0");
+				//printf("%s\n",path);
+				sortCSVFile(entry->d_name, token , outdir);
+				kill(getpid(), SIGKILL);
+			}
+		}
+    	} 
+	int i;
+	for(i=0;i<procs;i++)
+	{
+		wait(NULL);
+	}
+	if(getpid() != idp)
+	{
+		kill(getpid(), SIGKILL);
+	}
+	close(p[1]);
+    	chdir("..");  
+    	closedir(dp);     
+	free(path);
+}  
+ 
+
 int main(int argc, char ** argv){
 /*
 	if(stdin == NULL){
@@ -453,8 +554,55 @@ int main(int argc, char ** argv){
 		return 0;
 	}
 */	
+	pipe(p);
+	int ipd = getpid();
+	int procNum=1;
+	printf("Initial PID: %d\nPIDS of all child processes: ", ipd);
+	fflush(stdout);
+	//printf("%d\n", argc);	
+	char * indir =  malloc(1000);
+	char * tempop = malloc(1000);
+	char * token = malloc(1000);
+	strcpy(token, argv[2]);
+	strcpy(indir, argv[4]);
+	strcpy(tempop, argv[6]);
+	//fprintf(stdout, "%s \n " , tempop);
 	FILE *file;
 
+	
+	if(argc == 3 && argv[1][0] == '-' && argv[1][1] == 'c' && argv[2] != NULL) {
+		strcpy(indir, "./");
+		strcpy(tempop, "./");
+		strcpy(token, argv[2]);
+	}
+	else if(argc == 5){
+		strcpy(token, argv[2]);		
+	if(argv[3][0] == '-' && argv[3][1] == 'd' && argv[4] != NULL){
+		strcpy(indir, argv[4]);
+		strcpy(tempop, "./");
+		
+	}
+	else if(argv[3][0] == '-' && argv[3][1] == 'o' && argv[4] != NULL){
+	
+		file = fopen(argv[4], "r");
+	if(file == NULL){
+		fprintf(stderr, "ERROR: <No Output Dir Found\n>");
+		return 0;
+	}
+	fclose(file);
+
+		strcpy(tempop, argv[4]);
+		strcpy(indir, "./");
+	}
+	else{
+		fprintf(stderr,"Incorrect format\n");
+		return 0;
+	}
+	}
+
+	else if(argc == 7)
+	{
+		strcpy(token, argv[2]);
 	if(argv[1] == NULL){
 		fprintf(stderr, "ERROR: <Expected -c \"item\">\n");	
 		//free(token);
@@ -469,8 +617,7 @@ int main(int argc, char ** argv){
 		return 0;
 	}
 	
-	char * token = strdup(argv[2]);
-
+	
 	
 	if(argv[3][0] != '-' && argv[3][1] != 'd' && argv[4] == NULL){
 		fprintf(stderr, "ERROR: <Expected -d \"dir\">\n");
@@ -486,17 +633,38 @@ int main(int argc, char ** argv){
 		return 0;
 	}
 
-	file = fopen( argv[6], "r");
+		strcpy(tempop, argv[6]);
+		strcpy(indir, argv[4]);
+	
+	file = fopen(argv[6], "r");
 	//printf(argv[6]);
 	if(file == NULL){
-		fprintf(stderr, "ERROR: <No File Found\n>");
+		fprintf(stderr, "ERROR: <No Output Dir Found\n>");
 		return 0;
 	}
-	
-	//printf("hi");	
-	sortCSVFile(argv[4], token,argv[6]);	
-	
+	fclose(file);
 
-	fclose(file);	
+	}
+
+	else{
+		fprintf(stderr, "ERROR: <Expected -c at least with -d or -o \"output\">\n");
+		return 0;
+	}
+
+	//printf("hi")
+	
+	char * outdirnew = malloc(1000);
+	realpath(tempop, outdirnew);
+	//printf("%s\n" ,tempop);
+	
+	//printf("%s, %s, %s", token, indir, temp90);	
+	//sortCSVFile(argv[4], token,tempop);	
+	printdir(token, indir , outdirnew, ipd);  
+	while(read(p[0], &depth, 1) != 0)
+	{
+		procNum = procNum+1;
+	}
+	printf("\nTotal number of processes: %d\n", procNum);
+	//fclose(file);	
 	return 0;
 }
